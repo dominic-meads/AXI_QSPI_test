9fd261d6706efe6ecacad85f68953b7ca665f26e
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2021-08-22 20:08:03 -0700

    Create AXI_QSPI_top_wrapper.vhd

---
+++ src/AXI_QSPI_top_wrapper.vhd
@@ -0,0 +1,112 @@
+--Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2020.1 (win64) Build 2902540 Wed May 27 19:54:49 MDT 2020
+--Date        : Sun Aug 22 19:48:52 2021
+--Host        : LAPTOP-######## running 64-bit major release  (build 9200)
+--Command     : generate_target AXI_QSPI_top_wrapper.bd
+--Design      : AXI_QSPI_top_wrapper
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity AXI_QSPI_top_wrapper is
+  port (
+    reset : in STD_LOGIC;
+    spi_rtl_io0_io : inout STD_LOGIC;
+    spi_rtl_io1_io : inout STD_LOGIC;
+    spi_rtl_sck_io : inout STD_LOGIC;
+    spi_rtl_ss_io : inout STD_LOGIC_VECTOR ( 0 to 0 );
+    sys_clock : in STD_LOGIC
+  );
+end AXI_QSPI_top_wrapper;
+
+architecture STRUCTURE of AXI_QSPI_top_wrapper is
+  component AXI_QSPI_top is
+  port (
+    sys_clock : in STD_LOGIC;
+    reset : in STD_LOGIC;
+    spi_rtl_io0_i : in STD_LOGIC;
+    spi_rtl_io0_o : out STD_LOGIC;
+    spi_rtl_io0_t : out STD_LOGIC;
+    spi_rtl_io1_i : in STD_LOGIC;
+    spi_rtl_io1_o : out STD_LOGIC;
+    spi_rtl_io1_t : out STD_LOGIC;
+    spi_rtl_sck_i : in STD_LOGIC;
+    spi_rtl_sck_o : out STD_LOGIC;
+    spi_rtl_sck_t : out STD_LOGIC;
+    spi_rtl_ss_i : in STD_LOGIC_VECTOR ( 0 to 0 );
+    spi_rtl_ss_o : out STD_LOGIC_VECTOR ( 0 to 0 );
+    spi_rtl_ss_t : out STD_LOGIC
+  );
+  end component AXI_QSPI_top;
+  component IOBUF is
+  port (
+    I : in STD_LOGIC;
+    O : out STD_LOGIC;
+    T : in STD_LOGIC;
+    IO : inout STD_LOGIC
+  );
+  end component IOBUF;
+  signal spi_rtl_io0_i : STD_LOGIC;
+  signal spi_rtl_io0_o : STD_LOGIC;
+  signal spi_rtl_io0_t : STD_LOGIC;
+  signal spi_rtl_io1_i : STD_LOGIC;
+  signal spi_rtl_io1_o : STD_LOGIC;
+  signal spi_rtl_io1_t : STD_LOGIC;
+  signal spi_rtl_sck_i : STD_LOGIC;
+  signal spi_rtl_sck_o : STD_LOGIC;
+  signal spi_rtl_sck_t : STD_LOGIC;
+  signal spi_rtl_ss_i_0 : STD_LOGIC_VECTOR ( 0 to 0 );
+  signal spi_rtl_ss_io_0 : STD_LOGIC_VECTOR ( 0 to 0 );
+  signal spi_rtl_ss_o_0 : STD_LOGIC_VECTOR ( 0 to 0 );
+  signal spi_rtl_ss_t : STD_LOGIC;
+begin
+AXI_QSPI_top_i: component AXI_QSPI_top
+     port map (
+      reset => reset,
+      spi_rtl_io0_i => spi_rtl_io0_i,
+      spi_rtl_io0_o => spi_rtl_io0_o,
+      spi_rtl_io0_t => spi_rtl_io0_t,
+      spi_rtl_io1_i => spi_rtl_io1_i,
+      spi_rtl_io1_o => spi_rtl_io1_o,
+      spi_rtl_io1_t => spi_rtl_io1_t,
+      spi_rtl_sck_i => spi_rtl_sck_i,
+      spi_rtl_sck_o => spi_rtl_sck_o,
+      spi_rtl_sck_t => spi_rtl_sck_t,
+      spi_rtl_ss_i(0) => spi_rtl_ss_i_0(0),
+      spi_rtl_ss_o(0) => spi_rtl_ss_o_0(0),
+      spi_rtl_ss_t => spi_rtl_ss_t,
+      sys_clock => sys_clock
+    );
+spi_rtl_io0_iobuf: component IOBUF
+     port map (
+      I => spi_rtl_io0_o,
+      IO => spi_rtl_io0_io,
+      O => spi_rtl_io0_i,
+      T => spi_rtl_io0_t
+    );
+spi_rtl_io1_iobuf: component IOBUF
+     port map (
+      I => spi_rtl_io1_o,
+      IO => spi_rtl_io1_io,
+      O => spi_rtl_io1_i,
+      T => spi_rtl_io1_t
+    );
+spi_rtl_sck_iobuf: component IOBUF
+     port map (
+      I => spi_rtl_sck_o,
+      IO => spi_rtl_sck_io,
+      O => spi_rtl_sck_i,
+      T => spi_rtl_sck_t
+    );
+spi_rtl_ss_iobuf_0: component IOBUF
+     port map (
+      I => spi_rtl_ss_o_0(0),
+      IO => spi_rtl_ss_io(0),
+      O => spi_rtl_ss_i_0(0),
+      T => spi_rtl_ss_t
+    );
+end STRUCTURE;
+
